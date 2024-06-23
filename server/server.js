2e4fc083ea94c1c9355e54fd3faaf30a997612ea
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-06-23 21:58:41 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connectDB = require("./connectDB");
+const Book = require("./models/Books");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// middleware | middlewares are functions that have access to objects res, req and next function
+app.use(cors()); // This middleware is used to enable Cross-Origin Resource Sharing (CORS)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // This middleware is used to parse incoming requests with JSON payloads || makes the data available on req.body
+app.use("/", (req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.get("/api/books", async (req, res) => {
+  try {
+    const data = await Book.find({}).limit(3);
+    res.json(data);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "An error occured while fetching the books." });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json("Success");
+});
+
+app.get("*", (req, res) => {
+  res.send("not found").status(404);
+});
+
+connectDB().then((response) => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`);
+  });
+});
